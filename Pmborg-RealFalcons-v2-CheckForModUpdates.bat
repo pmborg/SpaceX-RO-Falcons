5e24859e7d0c060ac4036ddfbe019b29b601b5c0
pmborg <pmborg@yahoo.com>
2020-11-22 13:33:14 +0000

    Upgrade BAT now also re-install Pmborg-RealFalcons

--- Pmborg-RealFalcons-v2-CheckForModUpdates.bat
+++ Pmborg-RealFalcons-v2-CheckForModUpdates.bat
@@ -2,4 +2,13 @@ echo Please make sure that:
 echo - ckan.exe GUI is not running.
 echo - and KSP.exe is not running.
 echo 
+
+echo "Upgrading all Dependency Mods:"
 ckan.exe upgrade --all
+
+echo "RE-INSTALLING LATEST: Pmborg-RealFalcons"
+set KSPTEMP=%TEMP%\ksp~%RANDOM%.tmp
+MKDIR %KSPTEMP%
+powershell -command "& { iwr https://github.com/pmborg/SpaceX-RO-Falcons/archive/main.zip -OutFile main.zip }"
+powershell.exe -NoP -NonI -Command "Expand-Archive 'main.zip' '%KSPTEMP%'"
+xcopy /Y /S %KSPTEMP%\SpaceX-RO-Falcons-main .
