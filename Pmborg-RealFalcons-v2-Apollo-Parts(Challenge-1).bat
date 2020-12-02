ed18d285e3443e40b982a217281fc8d43e77b261
pmborg <pmborg@yahoo.com>
2020-12-02 20:10:37 +0000

    fix: warning from B9PartSwitch and also add file for Challenge-1

---
+++ Pmborg-RealFalcons-v2-Apollo-Parts(Challenge-1).bat
@@ -0,0 +1,5 @@
+REM 
+REM This Allow all parts needed to NASA Apollo Missions:
+REM 
+
+ckan.exe install --headless --allow-incompatible --no-recommends HullcamVDSContinued BluedogDB RealScaleBoosters Fasa
