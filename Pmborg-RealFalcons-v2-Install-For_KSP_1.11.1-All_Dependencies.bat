5480343cf1ab684fb10a699ae8f44edbbd6ab498
pmborg <pmborg@yahoo.com>
2020-12-19 23:42:56 +0000

    Allow install on KSP1.11

--- Pmborg-RealFalcons-v2-Install-For_KSP_1.11.1-All_Dependencies.bat
+++ Pmborg-RealFalcons-v2-Install-For_KSP_1.11.1-All_Dependencies.bat
@@ -109,14 +109,14 @@ REM INSTALL: ISS (International SpaceStation) Dep-1:
 	xcopy /S %KSPTEMP%\GameData\Science818 GameData\Science818
 	move SpaceX_Launch_Towers-1.7.3.zip %KSPTEMP%
 
-	REM INSTALL: KopernicusBE_1101
+	REM INSTALL: KopernicusBE_111
 	powershell -command "& { iwr https://github.com/R-T-B/Kopernicus/releases/download/UBE-release-56/KopernicusBE_111_Release56.zip -OutFile KopernicusBE_111_Release56.zip }"
 	powershell.exe -NoP -NonI -Command "Expand-Archive 'KopernicusBE_111_Release56.zip' '%KSPTEMP%'"
 	mkdir GameData\Kopernicus
 	mkdir GameData\ModularFlightIntegrator
 	xcopy /S /Y %KSPTEMP%\GameData\Kopernicus GameData\Kopernicus
 	xcopy /S /Y %KSPTEMP%\GameData\ModularFlightIntegrator GameData\ModularFlightIntegrator
-	move KopernicusBE_1101_Release56.zip %KSPTEMP%
+	move KopernicusBE_111_Release56.zip %KSPTEMP%
 
 	REM Move Not Needed/Incompatible Stuff to "NONEED" directory:
 	set NONEED=noneed~%RANDOM%.todelete
